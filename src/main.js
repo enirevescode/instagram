f3aa2bdb1c165678a6f573e914797e9374b56f00
enirevescode <enirevescode@gmail.com>
2023-10-09 11:43:20 +0200

    mise en place appli instagram

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Antd from "ant-design-vue"
+import App from './App.vue'
+import router from './router'
+
+import
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(Antd)
+app.use(router)
+
+app.mount('#app')

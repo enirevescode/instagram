5b443aee0ac67fe8ba23addccce8d6a047ee5fba
enirevescode <enirevescode@gmail.com>
2023-10-10 21:19:57 +0200

    Profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ContainerNav from './ContainerNav.vue';
+
+</script>
+
+<template>
+    <ContainerNav>
+        <div class="profile-container">
+            <h1>USERBAR</h1>
+            <h1>IMAGES GALLERY</h1>
+        </div>
+       
+    </ContainerNav>
+</template>
+
+<style scoped>
+.profile-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 0;
+}
+</style>
